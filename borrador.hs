bce3933d7478a14244cf77c7273f3f4bcf593f99
ralusama19 <ralucris.v@gmail.com>
2016-12-12 02:27:02 +0100

    almost done...

--- borrador.hs
+++ borrador.hs
@@ -1,29 +1,17 @@
+main = do
+	tipus <- getLine
+	opcio <- getLine
+	handle <- openFile "programhs.txt" ReadMode
+	program <- hGetContents handle  
+	g <- newStdGen
+	if opcio == 1
+	then if tipus == 0
+		 then putStrLn $ opcioManual ((read llistain::[Int]) ++ randoms g) (read program :: Command Int)
+		 else putStrLn $ opcioManual ((read llistain::[Double]) ++ randoms g) (read program :: Command Double)
+	else putStrLn "no valid option"
+	hClose handle
 
-asign :: (Num a, Ord a) => SymTable a -> Ident -> NExpr a -> ((Either String [a]),SymTable a, [a])
-
-throwErr :: (Num a, Ord a) => SymTable a -> [a] -> String -> ((Either String [a]),SymTable a, [a])
-
-printExpr :: (Num a, Ord a) => SymTable a -> NExpr a -> ((Either String [a]),SymTable a, [a])
-
-empty :: (Num a, Ord a) => SymTable a -> Ident a -> ((Either String [a]),SymTable a, [a])
-
-push :: (Num a, Ord a) => SymTable a -> Ident -> NExpr a -> ((Either String [a]),SymTable a, [a])
-
-pop :: (Num a, Ord a) => SymTable a -> Ident -> Ident -> ((Either String [a]),SymTable a, [a])
-
-size :: (Num a, Ord a) => SymTable a -> Ident -> Ident -> ((Either String [a]),SymTable a, [a])
-
-
-interpretCommand :: (Num a, Ord a) => SymTable a -> [a] -> Command a -> ((Either String [a]),SymTable a, [a])
-interpretCommand t i (Asssign s expr) = asign t s expr
-interpretCommand t i (Input s)
-  | null i = throwErr "empty stack" t i
-  | otherwise = asign t s (Const (head i))
-interpretCommand t i (Print expr) = printExpr t expr
-interpretCommand t i (Empty s) = empty t s
-interpretCommand t i (Push s expr) = push t s expr
-interpretCommand t i (Pop s var) = pop t s var
-interpretCommand t i (Size s var) = size t s var
-interpretCommand t i (Seq l) = foldl () 
-interpretCommand t i (Cond expr thencom elsecom)
-interpretCommand t i (Loop expr com)
+opcioManual :: (Num a, Ord a) => [a] -> Command a -> String
+opcioManual input program = case interpretProgram input program of
+	Left s -> s
+	Right x -> show x
--- prac.hs
+++ prac.hs
@@ -36,7 +36,7 @@ data SymTable a = Variable Ident a (SymTable a) (SymTable a)
 
 class Evaluable e where
 	eval :: (Num a, Ord a) => (Ident -> Maybe a) -> (e a) -> (Either String a)
-	--typeCheck :: (Ident -> String) -> (e a) -> Bool
+	typeCheck :: (Ident -> String) -> (e a) -> Bool
 
 getValue :: SymTable a -> Ident -> (Maybe a)
 getValue TaulaBuida k = Nothing
@@ -64,7 +64,7 @@ getType (Pila s l left right) k
 instance Evaluable NExpr where
 	eval f (Const x) = Right x
 	eval f (Var x)
-	  | f x == Nothing = Left "undefined variable"
+	  | f x == Nothing = Left "Error: undefined variable"
 	  | otherwise = Right (fromJust(f x))
 	eval f (Plus expA expB)= case (eval f expA, eval f expB) of
 		(Left a, Left b) -> Left $ a ++ ", " ++ b
@@ -81,6 +81,12 @@ instance Evaluable NExpr where
 		(_, Left b) -> Left b
 		(Left a, _) -> Left a
 		(Right a, Right b) -> Right $ a * b
+	
+	typeCheck f (Const x) = False
+	typeCheck f (Var x) = ("Variable" == f x)
+	typeCheck f (Plus expA expB) = typeCheck f expA && typeCheck f expB
+	typeCheck f (Minus expA expB) = typeCheck f expA && typeCheck f expB
+	typeCheck f (Times expA expB) = typeCheck f expA && typeCheck f expB
 
 instance Evaluable BExpr where
 	eval f (Gt expA expB) = case (eval f expA, eval f expB) of
@@ -107,6 +113,12 @@ instance Evaluable BExpr where
 		(Left a) -> Left a
 		(Right b) -> Right $ if b == 1 then 0 else 1
 
+	typeCheck f (Gt expA expB) = typeCheck f expA && typeCheck f expB
+	typeCheck f (Eq expA expB) = typeCheck f expA && typeCheck f expB
+	typeCheck f (OR expA expB) = typeCheck f expA && typeCheck f expB
+	typeCheck f (AND expA expB) = typeCheck f expA && typeCheck f expB
+	typeCheck f (NOT expr) = typeCheck f expr
+
 putVar :: (Num a, Ord a) => SymTable a -> Ident -> a -> SymTable a
 putVar TaulaBuida s x = Variable s x TaulaBuida TaulaBuida
 putVar (Variable ident valor l r) s x
@@ -131,12 +143,14 @@ putStack (Pila ident valor l r) s x
 
 getStack :: SymTable a -> Ident -> (Maybe [a])
 getStack TaulaBuida k = Nothing
-getStack (Variable s x left right) k = Nothing
+getStack (Variable s x left right) k
+  | k < s = getStack left k
+  | k > s = getStack right k
+  | otherwise = Nothing
 getStack (Pila s l left right) k
   | k < s = getStack left k
   | k > s = getStack right k
   | otherwise = Just l
-
 
 asign :: (Num a, Ord a) => SymTable a -> Ident -> NExpr a -> [a] -> ((Either String [a]),SymTable a, [a])
 asign t s expr input = case eval (getValue t) expr of 
@@ -147,9 +161,11 @@ throwErr :: (Num a, Ord a) => String ->  SymTable a -> [a] -> ((Either String [a
 throwErr err t i = ((Left err),t,i)
 
 printExpr :: (Num a, Ord a) => SymTable a -> NExpr a -> [a] ->  ((Either String [a]),SymTable a, [a])
-printExpr t expr input = case eval (getValue t) expr of 
-														Left err -> (Left err, t, input)
-														Right x -> (Right (x:[]), t, input)
+printExpr t expr input 
+  | typeCheck (getType t) expr = case eval (getValue t) expr of 
+									Left err -> (Left err, t, input)
+									Right x -> (Right (x:[]), t, input)
+  | otherwise = (Left ("Error in PRINT: type error"), t, input)
 
 empty :: (Num a, Ord a) => SymTable a -> Ident -> [a] -> ((Either String [a]),SymTable a, [a])
 empty t s input = (Right [], putStack t s [], input)
@@ -157,22 +173,26 @@ empty t s input = (Right [], putStack t s [], input)
 push :: (Num a, Ord a) => SymTable a -> Ident -> NExpr a -> [a] -> ((Either String [a]),SymTable a, [a])
 push t s expr input = case eval (getValue t) expr of 
 													 Left err -> (Left err, t, input)
-													 Right x -> (Right [], putStack t s (x:cosa), input) where Just cosa = getStack t s
+													 Right x -> sortida where sortida = case getStack t s of
+													 										Just cosa -> (Right [], putStack t s (x:cosa), input)
+													 				 						Nothing -> (Left ("Error in PUSH: undefined variable "++s), t, input)
 
 pop :: (Num a, Ord a) => SymTable a -> Ident -> Ident -> [a] -> ((Either String [a]),SymTable a, [a])
 pop t s dest input
-	| getType t s == "Pila" && getValue t s == Nothing = (Left "empty stack", t, input)
+	| getType t s == "Pila" && getValue t s == Nothing = (Left ("Error in POP: empty stack "++s), t, input)
 	| getType t s == "Pila" 						   = (Right [], putStack (putVar t dest cosa) s (tail cosa2), input)
-	| getType t s == "" = (Left "undefined variable", t, input)
-	| otherwise = (Left "type error", t, input)
+	| getType t s == "" = (Left ("Error in POP: undefined variable "++s), t, input)
+	| otherwise = (Left ("Error in POP: type error with "++s), t, input)
 	where Just cosa = getValue t s
 	      Just cosa2 = getStack t s
 
 size :: (Num a, Ord a) => SymTable a -> Ident -> Ident -> [a] -> ((Either String [a]),SymTable a, [a])
 size t s d input
-  | getType t s == "Pila" = case getStack t s of Just x -> (Right [], putVar t d (fromIntegral(length x)), input)
-  | getType t s == "" = (Left "undefined variable", t, input)
-  | otherwise = (Left "type error", t, input)
+  | getType t s == "Pila" = case getStack t s of 
+  												Just x -> (Right [], putVar t d (fromIntegral(length x)), input)
+  												Nothing -> (Left ("Error in SIZE: undefined variable "++s), t, input)
+  | getType t s == "" = (Left ("BIS Error in SIZE: undefined variable "++s), t, input)
+  | otherwise = (Left ("Error in SIZE: type error with "++s), t, input)
 
 
 interpretAccumulator :: (Num a, Ord a) =>  ((Either String [a]),SymTable a, [a]) -> Command a -> ((Either String [a]),SymTable a, [a])
@@ -193,11 +213,15 @@ interpretCommand t i (Pop s var) = pop t s var i
 interpretCommand t i (Size s var) = size t s var i
 interpretCommand t i (Seq l) = foldl (interpretAccumulator) ((Right []),t,i) l
 interpretCommand t i (Cond expr thencom elsecom) = case eval (getValue t) expr of
-													Right 1 -> interpretCommand t i thencom
-													Right 0 -> interpretCommand t i elsecom
+													Right 1 -> interpretAccumulator (Right [],t,i) thencom
+													Right 0 -> interpretAccumulator (Right [],t,i) elsecom
+													Left x -> (Left x, t, i)
 interpretCommand t i (Loop expr com) = case (eval (getValue t) expr) of
-													Right 1 -> interpretCommand (table (interpretCommand t i com)) (input (interpretCommand t i com)) (Loop expr com)
-
+													Right 1 -> interpretAccumulator ((Right outacc),(table cosa),(input cosa)) (Loop expr com)
+													Right 0 -> (Right [], t, i)
+													Left x -> (Left x, t, i)
+													where cosa = interpretAccumulator (Right [],t,i) com
+													      Right outacc = output cosa
 
 interpretProgram:: (Num a, Ord a) => [a] -> Command a -> (Either String [a])
 interpretProgram input com = output (interpretCommand TaulaBuida input com)
@@ -245,30 +269,73 @@ table (_,x,_) = x
 input :: (a,b,c) -> c
 input (_,_,x) = x
 
-import Control.Monad.Loops (unfoldM)
-
-readInts :: IO [Int]
-readInts = unfoldM $ fmap (check . read) getLine
-  where check x = if x == 0 then Nothing else Just x
-
-readDubs :: IO [Double]
-readDubs = unfoldM $ fmap (check . read) getLine
-  where check x = if x == 0 then Nothing else Just x
-
 main = do
-	tipus <- getLine
-	opcio <- getLine
+	tp <- getLine
+	op <- getLine
+	let tipus = read tp::Int
+	    opcio = read op::Int
+	    aux = "[]"
+	    llistaINT = read aux::[Int]
+	    llistaDUB = read aux::[Double]
 	handle <- openFile "programhs.txt" ReadMode
 	program <- hGetContents handle  
-	if tipus == 0 then
-		g <- newStdGen
-		
-		let llistaRand = randoms g
+	g <- newStdGen
+	if opcio == 0
+		then do llistain <- getLine
+			if tipus == 0
+				then putStrLn $ opcioManual ((read llistain::[Int]) ++ randoms g) (read program :: Command Int)
+				else putStrLn $ opcioManual ((read llistain::[Double]) ++ randoms g) (read program :: Command Double)
+		else if opcio == 1
+			then if tipus == 0
+				then putStrLn $ opcioRandom (llistaINT++randoms g) (read program :: Command Int)
+				else putStrLn $ opcioRandom (llistaDUB++randoms g) (read program :: Command Double)
+			else do k <- getLine
+				let tests = read k::Int
+				if tipus == 0
+					then putStrLn $ opcioBulk tests (llistaINT++randoms g) (read program :: Command Int)
+					else putStrLn $ opcioBulk tests (llistaDUB++randoms g) (read program :: Command Double)
+	hClose handle
 
-	else if tipus == 1 then
-		else
-			
-	case interpretProgram input program of
-	Left s -> putStrLn s
-	Right x -> putStrLn $ show x
-    hClose handle
+opcioManual :: (Num a, Ord a, Show a) => [a] -> Command a -> String
+opcioManual i program = case interpretProgram i program of
+	Left s -> s
+	Right x -> show x
+
+opcioRandom :: (Num a, Ord a, Show a) => [a] -> Command a -> String
+opcioRandom i com = "Entrada: " ++ show (input cosa) ++ "\n" ++ "Sortida: " ++ show (output cosa) ++ "\n" ++ "Instruccions: " ++ show (table cosa) ++ "\n" where cosa = interpretProgramAux i com
+
+opcioBulk :: (Num a, Ord a, Show a) => Int -> [a] -> Command a -> String
+opcioBulk k = opcioRandom
+
+
+interpretAccumulatorAux :: (Num a, Ord a) =>  ((Either String [a],SymTable a, [a]), Int, [a]) -> Command a -> ((Either String [a],SymTable a, [a]), Int, [a])
+interpretAccumulatorAux ((Left err, t, i),k,l) = ((Left err, t, i),k,l)
+interpretAccumulatorAux acc com = case interpretCommandAux (table acc) (input acc) com of 
+																					((Left err, _, _), inst, usedinput) -> ((Left err, table acc, input acc), (table acc)+inst, (input acc)++usedinput)
+																					((Right l, t, i ), inst, usedinput) -> ((Right (outacc++l), t, i), (table acc)+inst, (input acc)++usedinput)
+																					where Right outacc = output output acc
+
+
+
+interpretCommandAux :: (Num a, Ord a) => SymTable a -> [a] -> Command a -> ((Either String [a],SymTable a, [a]), Int, [a])
+interpretCommandAux t i (Assign s expr) = (asign t s expr i, 1, [])
+interpretCommandAux t i (Input s) = (asign t s (Const (head i)) (tail i), 1, (head i):[])
+interpretCommandAux t i (Print expr) = (printExpr t expr i,1, [])
+interpretCommandAux t i (Empty s) = (empty t s i, 1, [])
+interpretCommandAux t i (Push s expr) = (push t s expr i, 1, [])
+interpretCommandAux t i (Pop s var) = (pop t s var i, 1, [])
+interpretCommandAux t i (Size s var) = (size t s var i, 1, [])
+interpretCommandAux t i (Seq l) = foldl (interpretAccumulatorAux) (((Right []),t,i), 0, []) l
+interpretCommandAux t i (Cond expr thencom elsecom) = case eval (getValue t) expr of
+													Right 1 -> interpretAccumulatorAux ((Right [],t,i), 0, []) thencom
+													Right 0 -> interpretAccumulatorAux ((Right [],t,i), 0, []) elsecom
+													Left x -> ((Left x, t, i),  0, [])
+interpretCommandAux t i (Loop expr com) = case (eval (getValue t) expr) of
+													Right 1 -> interpretAccumulatorAux (((Right outacc),(table cosa),(input cosa)),  0, []) (Loop expr com)
+													Right 0 -> ((Right [], t, i),  0, [])
+													Left x -> ((Left x, t, i),  0, [])
+													where cosa = interpretAccumulatorAux ((Right [], t, i),  0, []) com
+													      Right outacc = output cosa
+
+interpretProgramAux :: (Num a, Ord a) => [a] -> Command a -> ((Either String [a]), Int, [a])
+interpretProgramAux input com = case (interpretCommandAux TaulaBuida input com) of ((out, t, ileft), steps, iused) -> (out, steps, iused)
